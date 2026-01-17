3d27cb537f166a786947e572467f37f79a1c1dc2
Kgad <kalisagad05@gmail.com>
2026-01-17 14:31:46 +0200

    Add frontend files and render.yaml for deployment

---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "./" : "/",
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+    dedupe: ["react", "react-dom"],
+  },
+  optimizeDeps: {
+    include: [
+      "react",
+      "react-dom",
+      "react/jsx-runtime",
+      "react/jsx-dev-runtime",
+    ],
+    esbuildOptions: {
+      jsx: "automatic",
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
+      transformMixedEsModules: true,
+    },
+  },
+  publicDir: "public",
+}));
